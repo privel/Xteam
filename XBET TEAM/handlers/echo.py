9267a10797f29ac06e21c0713f70c9eba79a77d8
vSpxd <44081190+privel@users.noreply.github.com>
2023-09-15 00:57:55 +0600

    Delete XBET TEAM directory

--- XBET TEAM/handlers/echo.py
+++
@@ -1,9 +0,0 @@
-from aiogram.dispatcher import FSMContext
-
-from loader import dp
-from aiogram import types
-
-@dp.message_handler(state='*')
-async def qwewqe(message: types.Message, state: FSMContext):
-    await state.finish()
-    await message.answer('❌ Некорректный ввод echo')
